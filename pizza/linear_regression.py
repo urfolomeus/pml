daa0348b46c815c786af23f997ec6f17853cf057
Alan Gardner <urfolomeus@gmail.com>
2021-01-23 14:56:05 +0000

    Second pass at linear regression

--- pizza/linear_regression.py
+++ pizza/linear_regression.py
@@ -1,22 +1,24 @@
 import numpy as np
 
 
-def predict(X, w):
+def predict(X, w, b):
     """
     X is a NumPy array of x coords
-    w is a number representing the "weight" or angle of the line
+    w is a number representing the "weight" or slope of the line
+    b is a number representing the "bias" or y-intercept of the line
 
     Try to predict the value of the y coord based on the value of the x coord
     and the weight of the line.
     """
-    return X * w
+    return X * w + b
 
 
-def loss(X, Y, w):
+def loss(X, Y, w, b):
     """
     X is a NumPy array of x coords
     Y is a NumPy array of y coords
-    w is a number representing the "weight" or angle of the line
+    w is a number representing the "weight" or slope of the line
+    b is a number representing the "bias" or y-intercept of the line
 
     We square the error so that:
         - we can be sure that it will be a positive number
@@ -25,7 +27,7 @@ def loss(X, Y, w):
 
     This is known as the `mean squared error`
     """
-    return np.average((predict(X, w) - Y) ** 2)
+    return np.average((predict(X, w, b) - Y) ** 2)
 
 
 def train(X, Y, iterations, lr):
@@ -34,18 +36,22 @@ def train(X, Y, iterations, lr):
     If it doesn't manage to converge before the given number of iterations, it
     throws an exception.
     """
-    w = 0
+    w = b = 0
 
     for i in range(iterations):
-        current_loss = loss(X, Y, w)
+        current_loss = loss(X, Y, w, b)
         print("Iteration %4d => Loss: %.6f" % (i, current_loss))
 
-        if loss(X, Y, w + lr) < current_loss:
+        if loss(X, Y, w + lr, b) < current_loss:
             w += lr
-        elif loss(X, Y, w - lr) < current_loss:
+        elif loss(X, Y, w - lr, b) < current_loss:
             w -= lr
+        elif loss(X, Y, w, b + lr) < current_loss:
+            b += lr
+        elif loss(X, Y, w, b - lr) < current_loss:
+            b -= lr
         else:
-            return w
+            return w, b
 
     raise Exception("Couldn't converge within %d iterations" % iterations)
 
@@ -54,8 +60,8 @@ def train(X, Y, iterations, lr):
 X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
 
 # Train the system
-w = train(X, Y, iterations=10000, lr=0.01)
-print("\nw=%.3f" % w)
+w, b = train(X, Y, iterations=10000, lr=0.01)
+print("\nw=%.3f, b=%.3f" % (w, b))
 
 # predict the number of pizzas
-print("Prediction: x=%d => y=%.2f" % (20, predict(20, w)))
+print("Prediction: x=%d => y=%.2f" % (20, predict(20, w, b)))
