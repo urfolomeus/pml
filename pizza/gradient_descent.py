68677f40ee6fb82a79517d10940ffe78f22d9dbb
Alan Gardner <urfolomeus@gmail.com>
2021-02-16 11:07:26 +0000

    [ch3] adding b

--- pizza/gradient_descent.py
+++ pizza/gradient_descent.py
@@ -30,18 +30,23 @@ def loss(X, Y, w, b):
     return np.average((predict(X, w, b) - Y) ** 2)
 
 
-def gradient(X, Y, w):
-    return 2 * np.average(X * (predict(X, w, 0) - Y))
+def gradient(X, Y, w, b):
+    w_gradient = 2 * np.average(X * (predict(X, w, b) - Y))
+    b_gradient = 2 * np.average(predict(X, w, b) - Y)
+    return (w_gradient, b_gradient)
 
 
 def train(X, Y, iterations, lr):
-    w = 0
+    w = b = 0
     for i in range(iterations):
-        print("Iteration %4d => Loss: %.10f" % (i, loss(X, Y, w, 0)))
-        w -= gradient(X, Y, w) * lr
-    return w
+        print("Iteration %4d => Loss: %.10f" % (i, loss(X, Y, w, b)))
+        w_gradient, b_gradient = gradient(X, Y, w, b)
+        w -= w_gradient * lr
+        b -= b_gradient * lr
+    return w, b
 
 
 X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
-w = train(X, Y, iterations=100, lr=0.001)
-print("\nw=%.10f" % w)
+w, b = train(X, Y, iterations=20000, lr=0.001)
+print("\nw=%.10f, b=%.10f" % (w, b))
+print("Prediction: x=%d => y=%.2f" % (20, predict(20, w, b)))
